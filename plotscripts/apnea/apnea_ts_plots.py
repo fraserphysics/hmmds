b3c92e8fa9ab2f3328274673d716facc2b142f2c
Andrew <andy@fraserphysics.com>
2021-03-12 21:00:17 -0700

    Reorganize plotting.  I don't like the structure yet.

--- plotscripts/apnea/apnea_ts_plots.py
+++ plotscripts/apnea/apnea_ts_plots.py
@@ -1,35 +1,47 @@
-""" apnea_ts_plots.py a03er_seg a03_lphr a01_lphr a12_lphr a03erA_plot
-         a03erN_plot a03HR_plot ApneaNLD
+""" apnea_ts_plots.py makes the first four figures for Chapter 6
+
+Here is an example of use:
+
+python apnea_ts_plots.py --data_dir ../../derived_data/apnea  ../../figs/apnea/a03erA.pdf
 
 This is the first line of the data file a03er_seg and the fields:
 
 3000.01 -0.65 -4814.0 -4002.0  -56.0     70.0
-time     ECG                   1000*ONR  O2sat
+seconds ECG                    1000*ONR  O2sat
 
 This is the first line of the data file a03.lphr and the fields:
 
 0.0   70.5882352941 2.88250231173
-time  lphr          bandpass hr
+minutes lphr        bandpass hr
+
 """
 import sys
 import argparse
 import os.path
 
-import numpy as np
+import numpy
 
-minutes_seconds = lambda x: '$%1d\!:\!%02d$' % (x / 60, x % 60
-                                               )  # Latex time format
+
+def minutes_seconds(x: int) -> str:
+    """Format seconds as minutes:seconds for LaTeX
+
+    Args:
+        x: Integer number of seconds since start of record
+
+    Returns:
+        string formatted for LaTeX
+    """
+    return r'$%1d\!:\!%02d$' % (x / 60, x % 60)
 
 
 def read_data(data_file):
     """Read in "data_file" as an array"""
-    f = open(data_file, 'r')
-    data = [[float(x) for x in line.split()] for line in f.readlines()]
-    f.close()
-    return np.array(data).T
+    with open(data_file, 'r') as _file:
+        data = [[float(x) for x in line.split()] for line in _file.readlines()]
+    return numpy.array(data).T
 
 
-def plot_ECG_ONR_O2(args, start: int, stop: int, xbounds: tuple,
+def plot_ECG_ONR_O2(args, start: int, stop: int, time_interval: tuple,
                     label_times: tuple, subplots):
     """Plot Electrocardiogram, oronasal airflow, and O2 Saturation
 
@@ -37,12 +49,12 @@ def plot_ECG_ONR_O2(args, start: int, stop: int, xbounds: tuple,
         args: Command line arguments (for data directory)
         start: Start plots at this line in the file a03er_seg
         stop:  Stop plots at this line in the file a03er_seg
-        xbounds: Time range for the plots in seconds
+        time_interval: Time range for the plots in seconds
         label_times: Put time labels at these times
         subplots: matplotlib.pyplot function
 """
-    fig, (axecg, axonr, axO2) = subplots(nrows=3, ncols=1, sharex=True)
-    seg = read_data(os.path.join(args.data_dir, 'a03er_seg'))
+    fig, (ax_ecg, ax_onr, ax_O2) = subplots(nrows=3, ncols=1, sharex=True)
+    a03er_segment = read_data(os.path.join(args.data_dir, 'a03er_seg'))
 
     def subplot(ax,
                 y_data,
@@ -51,22 +63,34 @@ def plot_ECG_ONR_O2(args, start: int, stop: int, xbounds: tuple,
                 ylim,
                 label_yvalues,
                 yscale=1):
-        ax.plot(seg[0, start:stop] / 60, y_data, 'k-')
+        """Put a trace on an axes
+
+        Args:
+            ax: Matplotlib axes
+            y_data: Y values to plot
+            y_label: Label for y axis
+            label_times: Labels for time axis
+            ylim: Range of y axis
+            label_yvalues: Place tick marks on y axis
+            yscale: Multiply y_data by this factor
+        """
+        seconds_per_minute = 60
+        ax.plot(a03er_segment[0, start:stop] / seconds_per_minute, y_data, 'k-')
         ax.set_ylabel(y_label)
         ax.set_xticks(label_times)
-        ax.set_xticklabels([minutes_seconds(x) for x in label_times])
-        ax.set_yticks(np.array(label_yvalues) * yscale)
+        ax.set_xticklabels([minutes_seconds(t) for t in label_times])
+        ax.set_yticks(numpy.array(label_yvalues) * yscale)
         ax.set_yticklabels([r'$% 2.0f$' % x for x in label_yvalues])
         ax.set_ylim(ylim)
-        ax.set_xlim(xbounds)
+        ax.set_xlim(time_interval)
         return
 
-    subplot(axecg, seg[1, start:stop] / 1000, r'$ECG$', [], (-0.015, 0.035),
-            np.arange(-10, 31, 10), 1e-3)
-    subplot(axonr, seg[4, start:stop] / 1000, r'$ONR$', [], (-15, 15),
-            np.arange(-10, 11, 10))
-    subplot(axO2, seg[5, start:stop], r'$SpO_2$', label_times, (55, 100),
-            np.arange(60, 101, 15))
+    subplot(ax_ecg, a03er_segment[1, start:stop] / 1000, r'$ECG$', [],
+            (-0.015, 0.035), numpy.arange(-10, 31, 10), 1e-3)
+    subplot(ax_onr, a03er_segment[4, start:stop] / 1000, r'$ONR$', [],
+            (-15, 15), numpy.arange(-10, 11, 10))
+    subplot(ax_O2, a03er_segment[5, start:stop], r'$SpO_2$', label_times,
+            (55, 100), numpy.arange(60, 101, 15))
 
     return fig
 
@@ -83,12 +107,12 @@ def register(func):
 
 @register
 def a03erA(args, subplots):
-    """For first figure in Chapter 6: "a03erA_plot.pdf"
+    """For first figure in Chapter 6: "a03erA.pdf"
 
     Plots of ECG, ONR and SpO2
     """
-    line_start = 45000
-    line_stop = 57005
+    line_start = 45000  # about 3450 seconds or 57.5 minutes
+    line_stop = 57005  # about 3570 seconds
     time_range = (57.5, 59.5)
     time_labels = (58, 59)
     fig = plot_ECG_ONR_O2(args, line_start, line_stop, time_range, time_labels,
@@ -98,12 +122,12 @@ def a03erA(args, subplots):
 
 @register
 def a03erN(args, subplots):
-    """For second figure in Chapter 6: "a03erN_plot.pdf"
+    """For second figure in Chapter 6: "a03erN.pdf"
 
     Plots of ECG, ONR and SpO2
     """
-    line_start = 120000
-    line_stop = 132001
+    line_start = 120000  # About 4200 seconds
+    line_stop = 132001  # About 4320 seconds
     time_range = (70, 72)
     time_labels = (70, 71, 72)
     fig = plot_ECG_ONR_O2(args, line_start, line_stop, time_range, time_labels,
@@ -113,35 +137,36 @@ def a03erN(args, subplots):
 
 @register
 def a03HR(args, subplots):
-    """For third figure in Chapter 6: "a03HR_plot.pdf"
+    """For third figure in Chapter 6: "a03HR.pdf"
 
+    Subplots of heart rate and O2 saturation
     """
     a03_lphr = read_data(os.path.join(args.data_dir, 'low_pass_heart_rate/a03'))
-    seg = read_data(os.path.join(args.data_dir, 'a03er_seg'))
+    a03er_segment = read_data(os.path.join(args.data_dir, 'a03er_seg'))
 
     xlim = (55, 65)  # For both
     ylim = (40, 90)  # For lphr only
 
-    fig, (axhr, axO2) = subplots(nrows=2, ncols=1, sharex=True)
+    fig, (ax_hr, ax_O2) = subplots(nrows=2, ncols=1, sharex=True)
 
-    axhr.plot(a03_lphr[0, :], a03_lphr[1, :], 'k-')
-    axhr.set_ylabel(r'$HR$')
-    label_yvalues = np.arange(45, 86, 10)
-    axhr.set_yticks(label_yvalues)
-    axhr.set_yticklabels(['$% 2.0f$' % x for x in label_yvalues])
-    axhr.set_xticks([])
-    axhr.set_ylim(ylim)
-    axhr.set_xlim(xlim)
+    ax_hr.plot(a03_lphr[0, :], a03_lphr[1, :], 'k-')
+    ax_hr.set_ylabel(r'$HR$')
+    label_yvalues = numpy.arange(45, 86, 10)
+    ax_hr.set_yticks(label_yvalues)
+    ax_hr.set_yticklabels(['$% 2.0f$' % x for x in label_yvalues])
+    ax_hr.set_xticks([])
+    ax_hr.set_ylim(ylim)
+    ax_hr.set_xlim(xlim)
 
-    axO2.plot(seg[0, :] / 60, seg[5, :], 'k-')
-    axO2.set_ylabel(r'$SpO_2$')
-    label_times = np.arange(55, 66, 5)
-    axO2.set_xticks(label_times)
-    axO2.set_xticklabels([minutes_seconds(x) for x in label_times])
-    label_yvalues = np.arange(60, 101, 10)
-    axO2.set_yticks(label_yvalues)
-    axO2.set_yticklabels(['$% 2.0f$' % x for x in label_yvalues])
-    axO2.set_xlim(xlim)
+    ax_O2.plot(a03er_segment[0, :] / 60, a03er_segment[5, :], 'k-')
+    ax_O2.set_ylabel(r'$SpO_2$')
+    label_times = numpy.arange(55, 66, 5)
+    ax_O2.set_xticks(label_times)
+    ax_O2.set_xticklabels([minutes_seconds(x) for x in label_times])
+    label_yvalues = numpy.arange(60, 101, 10)
+    ax_O2.set_yticks(label_yvalues)
+    ax_O2.set_yticklabels(['$% 2.0f$' % x for x in label_yvalues])
+    ax_O2.set_xlim(xlim)
 
     return fig
 
@@ -150,7 +175,7 @@ def a03HR(args, subplots):
 def ApneaNLD(args, subplots):
     """ Two segments of Heart rate for the fourth figure in Chapter 6.
 
-    Need a01_lphr, a12_lphr
+    Subplots of heart rate for segments of a01 and a12
     """
     a01_lphr = read_data(os.path.join(args.data_dir, 'low_pass_heart_rate/a01'))
     a12_lphr = read_data(os.path.join(args.data_dir, 'low_pass_heart_rate/a12'))
@@ -158,23 +183,25 @@ def ApneaNLD(args, subplots):
 
     ax_a01.plot(a01_lphr[0, :], a01_lphr[1, :], 'k-')
     ax_a01.set_ylabel(r'$a01$HR')
-    xrng = np.arange(115, 126, 5)
-    ax_a01.set_xticks(xrng)
-    ax_a01.set_xticklabels([minutes_seconds(x) for x in xrng])
-    yrng = np.arange(40, 101, 20)
-    ax_a01.set_yticks(yrng)
-    ax_a01.set_yticklabels(['$% 3.0f$' % x for x in yrng])
+    t_label_values = numpy.arange(115, 126, 5)
+    ax_a01.set_xticks(t_label_values)
+    ax_a01.set_xticklabels([minutes_seconds(x) for x in t_label_values])
+    y_label_values = numpy.arange(40, 101, 20)
+    ax_a01.set_yticks(y_label_values)
+    ax_a01.set_yticklabels(['$% 3.0f$' % x for x in y_label_values])
+    # Crop the plot
     ax_a01.set_ylim(40, 100)
     ax_a01.set_xlim(115, 125)
 
     ax_a12.plot(a12_lphr[0, :], a12_lphr[1, :], 'k-')
     ax_a12.set_ylabel(r'$a12$HR')
-    xrng = np.arange(570, 577, 5)
-    ax_a12.set_xticks(xrng)
-    ax_a12.set_xticklabels([minutes_seconds(x) for x in xrng])
-    yrng = np.arange(40, 81, 20)
-    ax_a12.set_yticks(yrng)
-    ax_a12.set_yticklabels(['$% 3.0f$' % x for x in yrng])
+    t_label_values = numpy.arange(570, 577, 5)
+    ax_a12.set_xticks(t_label_values)
+    ax_a12.set_xticklabels([minutes_seconds(x) for x in t_label_values])
+    y_label_values = numpy.arange(40, 81, 20)
+    ax_a12.set_yticks(y_label_values)
+    ax_a12.set_yticklabels(['$% 3.0f$' % x for x in y_label_values])
+    # Crop the plot
     ax_a12.set_ylim(40, 80)
     ax_a12.set_xlim(568, 577)
 
@@ -190,7 +217,7 @@ def main(argv=None):
         argv = sys.argv[1:]
 
     parser = argparse.ArgumentParser(
-        description='Make a figure illustrating apnea data')
+        description='Make one of the figures illustrating apnea data')
     parser.add_argument('--data_dir',
                         type=str,
                         default='../../derived_data/apnea',
@@ -200,9 +227,10 @@ def main(argv=None):
         action='store_true',
         help='display figure in pop-up window rather than storing it as a pdf')
 
-    parser.add_argument('fig_path',
-                        type=str,
-                        help='Attach "pdf" to this name for figure file')
+    parser.add_argument(
+        'fig_path',
+        type=str,
+        help='Path for storing the result, eg, ./../figs/apnea/a03HR.pdf')
     args = parser.parse_args(argv)
 
     import matplotlib
