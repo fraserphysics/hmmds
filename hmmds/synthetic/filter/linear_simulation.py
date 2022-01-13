10cf85c83b9349c1f7149978506472e87f7896e5
Andrew <andy@fraserphysics.com>
2022-01-12 21:55:58 -0700

    Moved varying arrays here from hmm.  Write filtered covariances.
    I think filtered covariances are too big.

--- hmmds/synthetic/filter/linear_simulation.py
+++ hmmds/synthetic/filter/linear_simulation.py
@@ -49,7 +49,7 @@ def parse_args(argv):
     parser.add_argument('--covariance',
                         type=float,
                         nargs=3,
-                        default=[.01, 0, .01],
+                        default=[.25, 0, .25],
                         help='Initial covariance components (1,1), (1,2), and (2,2)')
     parser.add_argument('--omega',
                         type=float,
@@ -65,16 +65,16 @@ def parse_args(argv):
                         help='Sample frequency')
     parser.add_argument('--b',
                         type=float,
-                        default=.01,
-                        help='System noise variance')
+                        default=.1,
+                        help='System noise multiplier')
     parser.add_argument('--c',
                         type=float,
                         default=0.5,
-                        help='Observation multiplier')
+                        help='Observation map')
     parser.add_argument('--d',
                         type=float,
-                        default=0.09,
-                        help='Observation noise variance')
+                        default=0.2,
+                        help='Observation noise multiplier')
     parser.add_argument('--random_seed', type=int, default=3)
     parser.add_argument('xfile',
                         type=argparse.FileType('w', encoding='utf-8'),
@@ -83,6 +83,9 @@ def parse_args(argv):
                         type=argparse.FileType('w', encoding='utf-8'),
                         help='Write y data to this file')
     parser.add_argument('filtered',
+                        type=argparse.FileType('w', encoding='utf-8'),
+                        help='Write filtered data to this file')
+    parser.add_argument('covariances',
                         type=argparse.FileType('w', encoding='utf-8'),
                         help='Write filtered data to this file')
     return parser.parse_args(argv)
@@ -105,29 +108,34 @@ def main(argv=None):
         [numpy.cos(args.omega*args.dt), numpy.sin(args.omega*args.dt)],
         [-numpy.sin(args.omega*args.dt), numpy.cos(args.omega*args.dt)]
         ]) * numpy.exp(-args.a * args.dt)
-    b = numpy.eye(2) * args.b
+    b = numpy.eye(2) * args.b  # State noise is b * Normal(0,I)
     c = numpy.array([[args.c, 0.0],])
-    d = numpy.array([args.d],dtype=numpy.float64)
+    d = numpy.array([args.d],dtype=numpy.float64) # Observation noise is c * Normal(0,I)
     mean = numpy.array(args.mean)
     covariance = numpy.array([
         [args.covariance[0], args.covariance[1]],
         [args.covariance[1], args.covariance[0]]
         ])
 
-    system = hmm.state_space.LinearGaussian(a, b, c, d, mean.copy(), covariance.copy(), rng)
+    initial_dist = hmm.state_space.MultivariateNormal(mean, covariance, rng)
+    system = hmm.state_space.LinearGaussian(a, b, c, d, rng)
 
-    system.simulate_n_steps(args.n_samples)
-    system.filter()  # Run Kalman filter on simulated observations
+    x, y = system.simulate_n_steps(initial_dist, args.n_samples)
+    means, covariances = system.filter(initial_dist, y)  # Run Kalman filter on simulated observations
 
     # Write the results
     for t in range(0, args.n_samples):
-        args.xfile.write(f'{system.x[t,0]:6.3f} {system.x[t,1]:6.3f}\n')
+        args.xfile.write(f'{x[t,0]:6.3f} {x[t,1]:6.3f}\n')
 
     for t in range(0, args.n_samples):
-        args.filtered.write(f'{system.filtered_x[t,0]:6.3f} {system.filtered_x[t,1]:6.3f}\n')
+        args.filtered.write(f'{means[t,0]:6.3f} {means[t,1]:6.3f}\n')
 
     for t in range(0, args.n_samples):
-        args.yfile.write(f'{system.y[t,0]:6.3f}\n')
+        args.covariances.write(f"""{t:3d} {covariances[t,0,0]:6.3f} {covariances[t,0,1]:6.3f}
+    {covariances[t,1,0]:6.3f} {covariances[t,1,1]:6.3f}\n""")
+
+    for t in range(0, args.n_samples):
+        args.yfile.write(f'{y[t,0]:6.3f}\n')
     return 0
 
 
