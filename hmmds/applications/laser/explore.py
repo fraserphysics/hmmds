464768a434ec13b537f45cabcecc1c5256ffead9
Andrew <andy@fraserphysics.com>
2022-02-23 14:39:05 -0700

    Analysis of Lorenz system fixed point.
    Notes in figures.tex.  Return map in explore.py.

--- hmmds/applications/laser/explore.py
+++ hmmds/applications/laser/explore.py
@@ -4,18 +4,116 @@ Derived from https://www.pythonguis.com/tutorials/plotting-pyqtgraph/
 
 """
 
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+import argparse
+import pdb
+
 from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QSizePolicy, QSlider, QSpacerItem, \
     QVBoxLayout, QWidget, QPushButton
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-import sys  # We need sys so that we can pass argv to QApplication
-import os
 
 import numpy
+import numpy.linalg
+import scipy.optimize
+
+import plotscripts.utilities
+
 from hmmds.synthetic.filter.lorenz_sde import lorenz_integrate
 
+def parse_args(argv):
+    """ Convert command line arguments into a namespace
+    """
+
+    parser = argparse.ArgumentParser(description='Explore 1-d map')
+    parser.add_argument('--show',
+                        action='store_false',
+                        help="display figure using Qt5")
+    return parser.parse_args(argv)
+def plot_for_r(r):
+    argv = sys.argv[1:]
+    args, _, pyplot = plotscripts.utilities.import_and_parse(parse_args, argv)
+    figure, (time, x_map) = pyplot.subplots(nrows=2, ncols=1, sharex=True)
+
+    n_x = 100
+    x_array = numpy.linspace(1/n_x,6,n_x)
+    x_initial = numpy.empty((n_x,3))
+    x_final =  numpy.empty((n_x,3))
+    t_array = numpy.empty(n_x)
+    
+    fixed_point = FixedPoint(r)
+
+    for i, delta_x in enumerate(x_array):
+        x_initial[i] = fixed_point.initial_state(delta_x)
+        t_array[i], x_final[i] = fixed_point.map_time(x_initial[i])
+    x_values = x_initial[:,0]
+    time.plot(x_values, t_array/fixed_point.period, label='t')
+    x_map.plot(x_values, x_final[:,0], label='final x')
+    x_map.plot(x_values, x_values, label='initial x')
+    for axis in time, x_map:
+        axis.legend()
+    
+    pyplot.show()
+    return 0
+
+class FixedPoint:
+    """Characterizes the focus of the Lorenz system at x_i > 0
+    """
+    def __init__(self, r):
+        s = 10.0
+        b = 8.0/3
+        self.r = r
+        root = numpy.sqrt(b*(r-1))
+        self.fixed_point = numpy.array([root, root, r-1])
+        df_dx = numpy.array([ # derivative of x_dot wrt x
+            [-s,  s,   0],
+            [1,   -1, -root],
+            [root, root, -b]])
+        values, right_vectors = numpy.linalg.eig(df_dx)
+        left_vectors = numpy.linalg.inv(right_vectors)
+        for i in range(3):
+            assert numpy.allclose(numpy.dot(left_vectors[i], df_dx), values[i]*left_vectors[i])
+            assert numpy.allclose(numpy.dot(df_dx, right_vectors[:,i]), values[i]*right_vectors[:,i])
+        assert values[0].imag == 0.0,f"First eigenvalue is not real: values={values}"
+        self.projection = numpy.dot(right_vectors[:,1:], left_vectors[1:,:]).real
+        # projection onto subspace of complex eigenvectors
+        self.image_2d = numpy.dot(numpy.array([[1,0,0],[0,0,1]]), self.projection)
+        # Components 0 and 2 of projection
+        assert numpy.allclose(numpy.dot(self.projection, right_vectors[:,-1]), right_vectors[:,-1])
+        self.omega = numpy.abs(values[-1].imag)
+        self.period = 2*numpy.pi/self.omega
+        self.relax = values[-1].real
+    def initial_state(self, delta_x):
+        """Find initial state that is distance delta_x from fixed point
+        """
+        coefficients = numpy.linalg.lstsq(self.image_2d, numpy.array([delta_x,0]),rcond=None)[0]
+        return numpy.dot(self.projection, coefficients) + self.fixed_point
+    def map_time(self, x_initial):
+        """Find time and position that x_initial maps to x[2] = r-1
+        """
+        h_max = 0.0025
+        tenths = numpy.empty((20,3))
+        t_step = self.period/10
+        # Integrate at least once because x_initial is on boundary
+        x_last = lorenz_integrate(x_initial, 0, t_step, h_max=h_max, r=self.r)
+        for i in range(20):
+            x_next = lorenz_integrate(x_last, 0, t_step, h_max=h_max, r=self.r)
+            if x_next[2] > self.r-1 > x_last[2]:
+                break
+            x_last = x_next
+        else:
+            raise RuntimeError("Failed to find bracket")
+        def func(time):
+            x_time = lorenz_integrate(x_last, 0, time, h_max=h_max, r=self.r)
+            result = x_time[2] - (self.r-1)
+            return result
+        delta_t = scipy.optimize.brentq(func, 0, t_step)
+        t_final = (i+1)*t_step + delta_t
+        x_final = lorenz_integrate(x_last, 0 , delta_t, h_max=h_max, r=self.r)
+        return t_final, x_final
 def make_data(r):
     big_t = 10000
     t_sample = 0.03
