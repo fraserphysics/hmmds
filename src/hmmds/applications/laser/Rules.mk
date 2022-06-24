d1a59db9d140e5cf29047f8627c69c2ca91007d0
Andrew <andy@fraserphysics.com>
2022-06-23 18:09:07 -0600

    Depend on building cython code.

--- src/hmmds/applications/laser/Rules.mk
+++ src/hmmds/applications/laser/Rules.mk
@@ -10,6 +10,9 @@ LASER_CODE = $(HMMDS)/applications/laser
 RAW_DATA = $(ROOT)/raw_data
 LP5DAT = $(RAW_DATA)/LP5.DAT
 
+# This is to ensure Cython code is compiled
+LORENZ_SDE = $(HMMDS)/synthetic/filter/lorenz_sde.flag
+
 # Define POWELL250 as an abbreviation
 POWELL250 = $(LASER_DATA)/ekf_powell250.parameters
 
@@ -19,13 +22,13 @@ PLOT_DATA = python $< --method skip --plot_data $@ --laser_data $(LP5DAT) $(word
 # $@ is the target
 # $(word 2, $^) is the second dependency
 
-$(LASER_DATA)/%.plot_data: $(LASER_CODE)/optimize_ekf.py $(LASER_DATA)/%.parameters
+$(LASER_DATA)/%.plot_data: $(LASER_CODE)/optimize_ekf.py $(LASER_DATA)/%.parameters $(LORENZ_SDE)
 	$(PLOT_DATA)
 
-$(LASER_DATA)/pf_%.plot_data: $(LASER_CODE)/optimize_particle.py $(LASER_DATA)/%.parameters
+$(LASER_DATA)/pf_%.plot_data: $(LASER_CODE)/optimize_particle.py $(LASER_DATA)/%.parameters $(LORENZ_SDE)
 	$(PLOT_DATA)
 
-$(LASER_DATA)/pf_ekf250.plot_data: $(LASER_CODE)/optimize_particle.py $(POWELL250)
+$(LASER_DATA)/pf_ekf250.plot_data: $(LASER_CODE)/optimize_particle.py $(POWELL250) $(LORENZ_SDE)
 	$(PLOT_DATA)
 
 # Define the action to build a plot_data target with override
@@ -33,38 +36,38 @@ OVERRIDE = python $< --method skip --plot_data $@ --laser_data $(LP5DAT) \
 --override_parameters $(word 3, $^) $(word 2, $^)
 
 $(LASER_DATA)/pf_opt_noise.plot_data: $(LASER_CODE)/optimize_particle.py $(POWELL250) \
-$(LASER_DATA)/pf_powell500.parameters
+$(LASER_DATA)/pf_powell500.parameters $(LORENZ_SDE)
 	$(OVERRIDE)
 
 $(LASER_DATA)/pf_hand_noise.plot_data: $(LASER_CODE)/optimize_particle.py $(POWELL250) \
-$(LASER_DATA)/pf_hand_noise.parameters
+$(LASER_DATA)/pf_hand_noise.parameters $(LORENZ_SDE)
 	$(OVERRIDE)
 
-$(LASER_DATA)/gui.plot_data: $(LASER_CODE)/optimize_ekf.py $(LASER_CODE)/explore.txt
+$(LASER_DATA)/gui.plot_data: $(LASER_CODE)/optimize_ekf.py $(LASER_CODE)/explore.txt $(LORENZ_SDE)
 	mkdir -p $(LASER_DATA)
 	python $< --method skip --plot_data $@ --parameter_type GUI_out --laser_data $(LP5DAT) \
 $(word 2, $^)
 
-$(LASER_DATA)/%.particle_data: $(LASER_CODE)/optimize_particle.py $(LASER_DATA)/%.parameters
+$(LASER_DATA)/%.particle_data: $(LASER_CODE)/optimize_particle.py $(LASER_DATA)/%.parameters $(LORENZ_SDE)
 	python $< --method skip --n_particles 300 --laser_data $(LP5DAT) --plot_data $@ \
 $(LASER_DATA)/$*.parameters
 
 $(LASER_DATA)/pf_hand_noise.parameters: $(LASER_DATA)/pf_powell500.parameters
 	sed -e "s/^state_noise.*/state_noise 0.2/" -e "s/^observation_noise.*/observation_noise 1.0/" < $< > $@
 
-$(LASER_DATA)/pf_powell500.parameters: $(LASER_CODE)/optimize_particle.py $(POWELL250)
+$(LASER_DATA)/pf_powell500.parameters: $(LASER_CODE)/optimize_particle.py $(POWELL250) $(LORENZ_SDE)
 	python $< --length 175 --n_particles 500 --method Powell --laser_data $(LP5DAT) $(POWELL250) $@
 
 # 5 hours -17.050910252989638 intermediate result, but final is -139.2018738220304?
-$(LASER_DATA)/particle.parameters: $(LASER_CODE)/optimize_particle.py $(POWELL250)
+$(LASER_DATA)/particle.parameters: $(LASER_CODE)/optimize_particle.py $(POWELL250) $(LORENZ_SDE)
 	python $< --length 175 --n_particles 100 --method Powell --laser_data $(LP5DAT) $(POWELL250) $@
 
 # Takes about 22 minutes
-$(LASER_DATA)/ekf_powell2876.parameters: $(LASER_CODE)/optimize_ekf.py $(POWELL250)
+$(LASER_DATA)/ekf_powell2876.parameters: $(LASER_CODE)/optimize_ekf.py $(POWELL250) $(LORENZ_SDE)
 	python $< --parameter_type parameter --laser_data $(LP5DAT) --length 2876 \
 --method Powell $(POWELL250) $@
 
-$(POWELL250): $(LASER_CODE)/optimize_ekf.py $(LASER_CODE)/explore.txt
+$(POWELL250): $(LASER_CODE)/optimize_ekf.py $(LASER_CODE)/explore.txt $(LORENZ_SDE)
 	mkdir -p $(LASER_DATA)
 	python $< --parameter_type GUI_out --laser_data $(LP5DAT) --length 250 \
 --method Powell $(word 2, $^) $@
--- src/hmmds/synthetic/filter/Rules.mk
+++ src/hmmds/synthetic/filter/Rules.mk
@@ -3,7 +3,7 @@
 # BUILD is where derived results go.
 
 FILTER_DATA = $(BUILD)/derived_data/synthetic/filter
-FILTER_CODE = $(ROOT)/hmmds/synthetic/filter
+FILTER_CODE = $(ROOT)/src/hmmds/synthetic/filter
 # FILTER_CODE is this directory
 
 FILTER_ARGS = --sample_rate 100 --sample_ratio 5 --n_fine 1000 --n_coarse 500 --a 0.01 --b 2.0 --d 25.0
@@ -15,10 +15,10 @@ $(FILTER_DATA)/%_data: $(FILTER_CODE)/%_simulation.py
 LORENZ_ARGS = --fudge 300 --dt 0.1 --b .1 --d 2.0 --sample_ratio 5 \
 --n_fine 201 --n_coarse 51 --random_seed 12
 
-$(FILTER_DATA)/lorenz_particle_data: $(FILTER_CODE)/lorenz_particle_simulation.py  $(FILTER_CODE)/lorenz_sde.cpython-38-x86_64-linux-gnu.so
+$(FILTER_DATA)/lorenz_particle_data: $(FILTER_CODE)/lorenz_particle_simulation.py  $(FILTER_CODE)/lorenz_sde.flag
 	mkdir -p $(FILTER_DATA)
 	python $< $(LORENZ_ARGS) $@
-$(FILTER_DATA)/lorenz_data: $(FILTER_CODE)/lorenz_simulation.py  $(FILTER_CODE)/lorenz_sde.cpython-38-x86_64-linux-gnu.so
+$(FILTER_DATA)/lorenz_data: $(FILTER_CODE)/lorenz_simulation.py  $(FILTER_CODE)/lorenz_sde.flag
 	mkdir -p $(FILTER_DATA)
 	python $< $(LORENZ_ARGS) $@
 $(FILTER_DATA)/distribution_data: $(FILTER_CODE)/distribution.py
@@ -29,8 +29,9 @@ $(FILTER_DATA)/log_likelihood_data: $(FILTER_CODE)/log_likelihood.py
 	mkdir -p $(FILTER_DATA)
 	python $<  --n_samples 1000 --n_b 10 --b_range .8 1.2 $@
 
-$(FILTER_CODE)/lorenz_sde.cpython-38-x86_64-linux-gnu.so: $(FILTER_CODE)/lorenz_sde.pyx $(FILTER_CODE)/build.py
+$(FILTER_CODE)/lorenz_sde.flag: $(FILTER_CODE)/lorenz_sde.pyx $(FILTER_CODE)/build.py
 	cd $(FILTER_CODE) ; python build.py build_ext --inplace
+	touch $@
 
 # Local Variables:
 # mode: makefile
