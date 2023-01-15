9bb4bd2ee6b22ab306211e728c3fa7bec8808e8b
Andrew M. Fraser <andy@orr.fraserphysics.com>
2023-01-15 16:46:33 -0700

    Builds skeleton/figures.pdf from ~ scratch in 8m36.653s
    $ rm -rf build/
    $ time make -j 16
    ...
    real	8m36.653s
    user	62m38.249s
    sys	0m8.833s

--- src/hmmds/synthetic/bounds/Rules.mk
+++ src/hmmds/synthetic/bounds/Rules.mk
@@ -7,16 +7,20 @@ BOUNDS_CODE = $(ROOT)/src/hmmds/synthetic/bounds
 # BOUNDS_CODE is this directory
 
 $(BOUNDS_DATA)/data_h_view:  $(BOUNDS_CODE)/h_cli.py
+	mkdir -p $(BOUNDS_DATA)
 	python $< $@
 
 $(BOUNDS_DATA)/toy_h: $(BOUNDS_CODE)/toy_h.py
+	mkdir -p $(BOUNDS_DATA)
 	python $< --n_t 200 --t_steps .02 .53 .125 --log_steps -3.5 -5.6 -.5 $@
 
 $(BOUNDS_DATA)/benettin: $(BOUNDS_CODE)/benettin.py
+	mkdir -p $(BOUNDS_DATA)
 	python $< --n_times=400 --n_runs=100 --n_relax 50 $@
 
 # This takes almost 8 minutes on cathcart
 $(BOUNDS_DATA)/like_lor: $(BOUNDS_CODE)/like_lor.py
+	mkdir -p $(BOUNDS_DATA)
 	python $< $@
 
 # Local Variables:
