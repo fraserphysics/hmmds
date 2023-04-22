43cc3175738cf3354dee44d6737218d67cf115c4
Andrew M. Fraser <andy@orr.fraserphysics.com>
2023-04-22 16:42:40 -0600

    New structure for states2hr.py runs on a01.

--- src/hmmds/applications/apnea/ECG/states2hr.py
+++ src/hmmds/applications/apnea/ECG/states2hr.py
@@ -26,6 +26,12 @@ def parse_args(argv):
 
     parser = argparse.ArgumentParser(
         description='Calculate heart rate from decoded states')
+    parser.add_argument('--likelihood',
+                        type=str,
+                        help='path to time series of p(y[t]|y[:t]).')
+    parser.add_argument('--censor',
+                        type=float,
+                        help='Threshold for censoring data')
     parser.add_argument('--samples_per_minute', type=int, default=10)
     parser.add_argument('--r_state',
                         type=int,
@@ -42,81 +48,128 @@ def parse_args(argv):
     return parser.parse_args(argv)
 
 
-def calculate(states: numpy.ndarray, r_state=20, outlier_state=0) -> dict:
-    """ Calculate heart rate and low pass heart rate
+def find_beats_and_periods(states: numpy.ndarray,
+                           r_state=35,
+                           outlier_state=0,
+                           likelihood=None,
+                           censor_fraction=0):
+    """Args:
+        states: State sequence from Viterbi decoding
+        r_state: Defines beat time
+        outlier_state: The state for ECG samples that are implausible.
+        likelihood: p(y[t]|y[:t])
+        censor_fraction: Fraction of intervals to reject based on likelihood
+
+    Return: The pair (array of integer times and periods, set of bad
+    intervals)
+
+    Bad intervals contain atypical or implausible signal segments.
+    Integer beat times at the start of such intervals tracks the set.
+
+    """
+    bad_intervals = set()
+
+    r_indices = numpy.nonzero(states == r_state)[0]
+    time_period = numpy.empty((len(r_indices) - 1, 2), dtype=int)
+    time_period[:, 0] = r_indices[:-1]
+    time_period[:, 1] = r_indices[1:] - r_indices[:-1]
+    for (time, period) in time_period:
+        if numpy.count_nonzero(states[time:time + period] == outlier_state) > 0:
+            bad_intervals.add(time)
+    if likelihood is None:
+        assert censor_fraction == 0
+        return time_period, bad_intervals
+    # ToDo: Code for censoring here
+    return time_period, bad_intervals
+
+
+def calculate(
+    time_period: numpy.ndarray,
+    bad_intervals: set,
+    time_out,
+    in_frequency=100 * PINT('Hz'),
+    out_frequency=2 * PINT('Hz')) -> dict:
+    """Calculate heart rate and low pass heart rate
 
     Args:
-        states: Decoded state sequence sampled at 100Hz
-        r_state: The state that corresponds to the R peak
-        outlier_state: The state for ECG samples that are implausible
+        time_period: Integer beat times and integer periods
+        bad_intervals: Subset of beat times with untrusted subsequent intervals
+        time_out: Pint time for length of result
+        in_frequency:
+        out_frequency:
 
-    Return: 
+    Return: Dict of heart rate time series and its sample frequency
+    with keys 'sample_frequency' and 'hr'.
+
     """
-
-    in_frequency = 100 * PINT('Hz')
-    out_frequency = 2 * PINT('Hz')
-
-    time_period_index = []
-    r_indices = numpy.nonzero(states == r_state)[0]
-    for next_index, r_index in zip(r_indices[1:], r_indices[:-1]):
-        if numpy.count_nonzero(
-                states[r_index:next_index] == outlier_state) == 0:
-            time_period_index.append([r_index, next_index - r_index])
-    time_period = numpy.array(time_period_index) / in_frequency
 
     # Create periods, an array of periods that is uniformly sampled at
     # out_frequency
-
-    last_time = (len(states) / 100) * PINT('seconds')
-    n_out = int((out_frequency * last_time).to('').magnitude)
+    n_out = int((out_frequency * time_out).to('').magnitude)
+    n_in = len(time_period)
     # Number of samples of inverse heart rate
-    periods = numpy.ones(n_out) * PINT('s')
-    if len(time_period_index) < 3:
-        # Worthless data in states
-        return {
-            'sample_frequency': out_frequency,
-            'hr': (1 / periods).to('1/minute')
-        }
+    out_periods = numpy.empty(n_out)
 
+    index_in = 0
+    next_time = 0
+    next_period = 0
+    last_time = 0
+    last_period = 0
+    dp_dt = 0
+
+    def next_good_period():
+        """Find the next good interval
+
+        Update values of index_in, last_time, next_time, last_period,
+        next_period
+
+        """
+        nonlocal last_time, next_time, last_period, next_period, index_in, dp_dt
+        for try_in in range(index_in, n_in):
+            time, period = time_period[try_in]
+            if time not in bad_intervals:
+                last_time = next_time
+                last_period = next_period
+                next_time = time
+                next_period = period
+                index_in = try_in + 1
+                dp_dt = (next_period - last_period) / (next_time - last_time)
+                return
+        index_in = n_in
+        last_time = next_time
+        last_period = next_period
+        dp_dt = 0
+
+    next_good_period()
+    assert index_in < n_in
+
+    # Assign first out_periods
     for k_out in range(n_out):
-        periods[k_out] = time_period[0, 1]
-        if k_out / out_frequency >= time_period[0, 0]:
+        if k_out / out_frequency >= next_time / in_frequency:
             k_start = k_out
             break
-    for k_out in range(n_out - 1, -1, -1):
-        periods[k_out] = time_period[-1, 1]
-        if k_out / out_frequency <= time_period[-1, 0]:
-            k_stop = k_out
-            break
+        out_periods[k_out] = next_period
 
-    # t_old        t_new
-    #   |            |
-    #------------------------------------------
-    #        |
-    #       t_k
-
-    i_time_period = 1
+    # Make values of last_* and next_* valid
+    next_good_period()
+    assert index_in < n_in
 
     # Assign a value to periods[k_out] for every k_out
-    for k_out in range(k_start, k_stop):
-        t_k = k_out / out_frequency
-
-        # Move t_old and t_new forward till t_k is in range
-        while t_k > time_period[i_time_period, 0]:
-            i_time_period += 1
-            if i_time_period >= len(time_period) - 1:
+    for k_out in range(k_start, n_out):
+        if k_out / out_frequency >= next_time / in_frequency:
+            next_good_period()
+            if index_in >= n_in:
+                for k in range(k_out, n_out):
+                    out_periods[k] = last_period
                 break
-        t_old, period_old = time_period[i_time_period - 1]
-        t_new, period_new = time_period[i_time_period]
-
-        # Calculate slope of period between t_old and t_new
-        drdt = (period_new - period_old) / (t_new - t_old)
-        periods[k_out] = period_old + (t_k - t_old) * drdt
+        t_k = k_out * in_frequency / out_frequency
+        out_periods[k_out] = last_period + (t_k - last_time) * dp_dt
 
     # Return results in beats per minute
+    out_periods /= in_frequency
     return {
         'sample_frequency': out_frequency,
-        'hr': (1.0 / periods).to('1/minute')
+        'hr': (1.0 / out_periods).to('1/minute')
     }
 
 
@@ -129,7 +182,12 @@ def main(argv=None):
     args = parse_args(argv)
     with open(args.states_file, 'rb') as _file:
         states = pickle.load(_file)
-    hr_dict = calculate(states, args.r_state)
+
+    input_sample_frequency = 100 * PINT('Hz')
+    time_out = len(states)/input_sample_frequency
+    
+    time_period, bad_intervals = find_beats_and_periods(states)
+    hr_dict = calculate(time_period, bad_intervals, time_out)
     with open(args.heart_rate_file, 'wb') as _file:
         pickle.dump(hr_dict, _file)
     return 0
