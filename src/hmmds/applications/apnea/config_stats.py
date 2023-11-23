eec94e87a08ee1b878127012216ad3000ba05cc1
andy <andy@penguin>
2023-11-22 20:51:08 -0800

    Use utilities.make_interval_pdfs in config_stats.py.
    Stopped using duplicate code in config_stats.py that was not
    consistent with code in utilities.

--- src/hmmds/applications/apnea/config_stats.py
+++ src/hmmds/applications/apnea/config_stats.py
@@ -25,9 +25,7 @@ from __future__ import annotations  # Enables, eg, self: Record
 import sys
 import pickle
 import argparse
-import os
 
-import numpy
 import scipy.interpolate
 import pint
 
@@ -62,7 +60,7 @@ def parse_args(argv):
 
 
 class Record:
-    """Holds properties of records and methods to calculate them
+    """Holds properties of records for calculating result.norm_avg
 
     """
 
@@ -82,115 +80,47 @@ class Record:
         self.hr_sample_frequency = _dict['sample_frequency']
         self.raw_hr = _dict['hr'].to('1/minute').magnitude
 
-        path = os.path.join(args.root, 'raw_data/apnea/summary_of_training')
-        self.minute2class = utilities.read_expert(path, record_name)
-
-    def normalize(self: Record, norm_avg):
-        """Make PSD sum of this record match others"""
-        self.raw_hr *= norm_avg / self.pass1.statistic_2()
-
-    def smooth(self: Record):
-        """Assign self.smoothed
-        """
-        # pylint: disable=attribute-defined-outside-init, invalid-name
-        self.fft_length = 131072
-        low_pass_width = 2 * numpy.pi / self.args.low_pass_period
-        self.RAW_HR = numpy.fft.rfft(self.raw_hr, self.fft_length)
-        self.smoothed = numpy.fft.irfft(
-            utilities.window(self.RAW_HR, 1 / self.hr_sample_frequency,
-                             0 * PINT('Hz'), low_pass_width))[:len(self.raw_hr)]
-
-    def find_peaks_intervals(
-            self: Record,
-            min_prominence,
-            peak_dict,
-            distance=0.417 * PINT('minutes'),
-            wlen=1.42 * PINT('minutes'),
-    ):
-        """ Put (peak, interval) pairs in peak_dict[class] for each detected peak.
-
-        Args:
-            min_prominence: Minimum for detected peaks
-            distance: Minimum time between peaks
-            wlen: Window length
-
-        """
-        s_f_hz = self.hr_sample_frequency.to('Hz').magnitude
-        s_f_cpm = int(s_f_hz * 60)
-        distance_samples = distance.to('seconds').magnitude * s_f_hz
-        wlen_samples = wlen.to('seconds').magnitude * s_f_hz
-
-        peak_times, properties = scipy.signal.find_peaks(
-            self.smoothed,
-            distance=distance_samples,
-            prominence=min_prominence,
-            wlen=wlen_samples)
-
-        for index in range(len(peak_times) - 1):
-            t_peak = peak_times[index]
-            prominence_t = properties['prominences'][index]
-            period_t = (peak_times[index + 1] - t_peak) / s_f_cpm
-            t_minute = t_peak // s_f_cpm
-            if t_minute >= len(self.minute2class):
-                break
-            class_t = self.minute2class[t_minute]
-            peak_dict[class_t].append((prominence_t, period_t))
-
 
 def main(argv=None):
-    """
+    """Write a pickle file with statistics of peaks in the heart rate
+       signal
+
     """
     if argv is None:  # Usual case
         argv = sys.argv[1:]
 
     args = parse_args(argv)
-    # setattr uses local variables pylint: disable=possibly-unused-variable
-    min_prominence = args.min_prominence
 
     if args.records:
         record_names = args.records
     else:
         record_names = args.a_names
 
-    records = dict((record_name, Record(args, record_name))
-                   for record_name in record_names)
+    result = argparse.Namespace()
 
     if args.normalize:
         norm_sum = 0.0
-        for record in records.values():
+        for record in (
+                Record(args, record_name) for record_name in record_names):
             norm_sum += record.pass1.statistic_2()
-        norm_avg = norm_sum / len(record_names)
-        for record in records.values():
-            record.normalize(norm_avg)
-        normalize = True
+        result.norm_avg = norm_sum / len(record_names)
+        result.normalize = True
     else:
-        normalize = False
-        norm_avg = None
+        result.normalize = False
+        result.norm_avg = None
 
-    # peak_dict holds (prominence, interval) pairs
-    peak_dict = {0: [], 1: []}
-    for record in records.values():
-        record.smooth()
-        record.find_peaks_intervals(record.args.min_prominence, peak_dict)
+    result.min_prominence = args.min_prominence
 
-    limit = 2.2  # No intervals longer than this for pdf ratio fit
-    sigma = 0.1  # Kernel width
-    _lambda = 0.06  # Regularize pdf ratio fit
-    # create a density_ratio.DensityRatio instance
-    pdf_ratio = utilities.make_density_ratio(peak_dict, limit, sigma, _lambda)
+    args.config = result  # Hack to enable use of utilities
+    result.pdf_ratio = utilities.make_interval_pdfs(args, record_names)
 
-    normal_pdf_spline = scipy.interpolate.CubicSpline(pdf_ratio.x,
-                                                      pdf_ratio.y,
-                                                      bc_type='natural',
-                                                      extrapolate=True)
+    result.normal_pdf_spline = scipy.interpolate.CubicSpline(result.pdf_ratio.x,
+                                                             result.pdf_ratio.y,
+                                                             bc_type='natural',
+                                                             extrapolate=True)
 
-    result = argparse.Namespace()
     result.apnea_pdf = utilities.apnea_pdf
     result.normal_pdf = utilities.normal_pdf
-    local = locals()
-    for key in '''normalize min_prominence norm_avg
-    normal_pdf_spline pdf_ratio'''.split():
-        setattr(result, key, local[key])
     with open(args.result_path, 'wb') as _file:
         pickle.dump(result, _file)
     return 0
