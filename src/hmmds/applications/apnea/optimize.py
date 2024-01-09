d31f2a8e330759d16adf2854ce479ccad67a0b0b
Andrew <andy@fraserphysics.com>
2024-01-09 16:51:31 -0700

    Copied changes for running optimization from branch respiration.
    I will delete the branch respiration.

--- src/hmmds/applications/apnea/optimize.py
+++ src/hmmds/applications/apnea/optimize.py
@@ -85,6 +85,14 @@ class Objective(dict):
             self.keys())
         self.model_keys = 'ar fs lpp rc rw rs pt vp ip'.split()
         self.variable_names = 'threshold lpp rc rw rs pt ip'.split()
+        self.variable_values = {}
+        for key, value in self.items():
+            if key in self.variable_names:
+                assert len(value) == 2
+                self.variable_values[key] = value[0]
+                continue
+            assert key in self.model_keys
+            assert isinstance(value, str)
 
     def make_model(self):
         """Make a trained HMM with parameters self[key] for key in
@@ -94,11 +102,10 @@ class Objective(dict):
         """
         model_list = ['varg4state_']
         for key in self.model_keys:
-            value = self[key]
             if key in self.variable_names:
-                model_list.append(f'{key}{value}')
+                model_list.append(f'{key}{self.variable_values[key]}')
             else:
-                model_list.append(f'{key}{value}')
+                model_list.append(f'{key}{self[key]}')
         model_list.append('_masked')
         model_path = pathlib.Path(self.model_dir, ''.join(model_list))
         subprocess.run(f'make {model_path}'.split(), check=True)
@@ -108,16 +115,16 @@ class Objective(dict):
         """Make a config file with prominence threshold self.pt
 
         """
-        path = f'norm_config{self["pt"]}.pkl'
+        path = f'norm_config{self.variable_values["pt"]}.pkl'
         subprocess.run(('make', path), check=True)
         return path
 
     def set(self, vector):
-        """Assign variable components to self
+        """Map vector to self.variable_values
         """
         assert len(vector) == len(self.variable_names)
-        for value, key in zip(vector, self.variable_names):
-            self[key] = value
+        for z, key in zip(vector, self.variable_names):
+            self.variable_values[key] = self[key][0] + z * self[key][1]
 
     def __call__(self, vector):
         self.set(vector)
@@ -126,38 +133,38 @@ class Objective(dict):
         counts = numpy.zeros(4)
         for record_name in self.records:
             model_record = utilities.ModelRecord(model_path, record_name)
-            model_record.classify(self['threshold'])
+            model_record.classify(self.variable_values['threshold'])
             counts += model_record.score()
         pathlib.Path(config_path).unlink()
         pathlib.Path(model_path).unlink()
         return counts[1] + counts[2]
 
+    def interpret(self, vector):
+        """Print contents of self and the parameters that vector maps
+        to.
+
+        """
+        self.set(vector)
+        for key, value in self.items():
+            if key in self.variable_names:
+                print(f'{key:8s}: {self.variable_values[key]:14.8f} <- {value}')
+            else:
+                print(f'{key:8s}: {value}')
+
 
 def interpret(path):
-    """
+    """Print information about result.  For example from ipython:
+
+    In [1]: from optimize import Objective
+    In [2]: import optimize
+    In [3]: optimize.interpret('v4s_opt')
+
     """
     with open(path, 'rb') as _file:
-        result, cost_initial, cost_final = pickle.load(_file)
-    
-    objective = Objective(
-        threshold=84.0,
-        ar='16',
-        fs='5',
-        lpp=28.9,
-        rc=14.1,
-        rw=4.58,
-        rs=.449,
-        pt=4.8,
-        vp='1.0',
-        ip=0.032,
-        model_dir='',
-        records='',
-    )
+        result, objective, cost_initial, cost_final = pickle.load(_file)
 
-    objective.set(result.x)
-    print(f'{cost_initial=} {cost_final=}')
-    for key, value in objective.items():
-        print(f'{key}: {value}')
+    print(f'{cost_initial=} {cost_final=}\n{result=}\n')
+    objective.interpret(result.x)
 
 
 def main(argv=None):
@@ -168,40 +175,71 @@ def main(argv=None):
 
     args = parse_args(argv)
 
+    # (a,b) "a" is the nominal value, and "b" is roughly the deviation
+    # to move from 3000 to 3500 errors
     objective = Objective(
-        threshold=84.0,
+        threshold=(2427.0, 700.0),
         ar='16',
         fs='5',
-        lpp=28.9,
-        rc=14.1,
-        rw=4.58,
-        rs=.449,
-        pt=4.8,
+        lpp=(28.9, 25.0),
+        rc=(14.1, 3.0),
+        rw=(4.58, 1.5),
+        rs=(.449, .02),
+        pt=(4.8, 2.5),
         vp='1.0',
-        ip=0.032,
+        ip=(0.045, 0.4),
         model_dir=args.model_dir,
         records=args.records,
     )
-    initial = numpy.array([objective[key] for key in objective.variable_names])
+    dim_x = len(objective.variable_names)
+    initial = numpy.zeros(dim_x)
     cost_initial = objective(initial)
     print(f'{cost_initial=}')
-    return 0
-    #                        'threshold lpp   rc    rw   rs   pt  ip'
-    lower_bounds = numpy.array([1.0e-4, 15.0, 12.0, 1.0, 0.2, 1, 0.01])
-    upper_bounds = numpy.array([1.0e4, 60.0, 16.0, 8.0, 1.0, 8, 100])
+
+    # For Nelder-Mead initial_simplex centered at zero with scale
+    # bigger then bumps
+    initial_simplex = numpy.ones((dim_x+1, dim_x))/dim_x
+    for i in range(dim_x):
+        initial_simplex[i+1,i] -= 1.1
+    initial_simplex *= .2
+
+    lower_bounds = -numpy.ones(dim_x) * 2.0
+    upper_bounds = numpy.ones(dim_x) * 2.0
     # Fail on a07 with pt=12.742645786248001 no peaks.  Ok with
-    # pt8.257354213751997
+    # pt8.257354213751997.  BFGS can't handle bounds
     bounds = scipy.optimize.Bounds(lower_bounds,
                                    upper_bounds,
                                    keep_feasible=True)
-    options = dict(ftol=.01, xtol=.1)
+
+    # Options are for CG or BFGS methods
+
+    # f(x_opt) \approx 3000
+    # Hope to get within 10 with dx .1 so
+    gtol = 10 / .1
+    # f is bumpy with dx=.1 so use
+    # finite_diff_rel_step = 0.2
+    eps = 0.2
+    options = dict(gtol=gtol, norm=2, eps=eps)
+
+    nm_options = dict(
+        inital_simplex=initial_simplex,
+        xatol=0.001,
+        fatol=5)
     result = scipy.optimize.minimize(objective,
-                                     initial,
-                                     method='powell',
-                                     bounds=bounds)
+                                     numpy.ones(dim_x),
+                                     method='Nelder-Mead',
+                                     options=nm_options)
     cost_final = objective(result.x)
     with open(args.out, 'wb') as _file:
-        pickle.dump((result, cost_initial, cost_final), _file)
+        pickle.dump((result, objective, cost_initial, cost_final), _file)
+
+    # print a summary of the result
+    interpret(args.out)
+
+    # Method Result
+    # BFGS   success: False
+    # CG     success: False.  'Desired error not necessarily achieved due to precision loss.'
+    # Powell success: True, but cost(result.x) > cost(initial)
 
     return 0
 
