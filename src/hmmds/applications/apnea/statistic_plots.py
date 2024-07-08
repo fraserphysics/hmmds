722e1bf47d9c61396d9671776ac56c4c845d39a1
Andrew <andy@fraserphysics.com>
2024-07-08 13:30:27 -0600

    Use numpy psd and sklearn.svm to estimate threshold functions.

--- src/hmmds/applications/apnea/statistic_plots.py
+++ src/hmmds/applications/apnea/statistic_plots.py
@@ -1,9 +1,16 @@
-"""statistic_plots.py.  Make plots for choosing statistics for pass1.
+"""fft_statistic_plots.py.  Make plots of statistics for psd -> threshold and pass1
 
-python statistic_plots.py --show foo.pdf
+python fft_statistic_plots.py statistics.pkl
 
-Plot 1. Plot the PSD of the unfiltered heart rate.
+Plots:
 
+    1a. PSDs of the unfiltered heart rate average of a records and c
+      records.
+    1b. Coefficients for threshold and pass1
+
+    2a. For each record, pass1 vs threshold
+
+    2b. For each record, best threshold vs fit threshold
 """
 
 import sys
@@ -13,11 +20,9 @@ import pickle
 import pint
 import numpy
 import numpy.linalg
-import scipy.signal
 
 import utilities
 import plotscripts.utilities
-import hmm.base
 
 PINT = pint.UnitRegistry()
 
@@ -26,125 +31,167 @@ def parse_args(argv):
     """ Convert command line arguments into a namespace
     """
 
-    parser = argparse.ArgumentParser(
-        description='Plots for choosing statistics')
-    parser.add_argument('--A_names',
+    parser = argparse.ArgumentParser(description='Plots of statistics')
+    parser.add_argument('pickle_threshold',
                         type=str,
-                        nargs='+',
-                        default=[f'a{x:02d}' for x in range(1, 21)])
-    parser.add_argument('--C_names',
+                        help='Path to statistics for threshold')
+    parser.add_argument('pickle_pass1',
                         type=str,
-                        nargs='+',
-                        default=[f'c{x:02d}' for x in range(1, 11)])
-    parser.add_argument('--X_names',
-                        type=str,
-                        nargs='+',
-                        default=[f'x{x:02d}' for x in range(1, 36)])
-    parser.add_argument('--B_names',
-                        type=str,
-                        nargs='+',
-                        default=[f'b{x:02d}' for x in range(1, 5)])
-    parser.add_argument('--sample_rate_in',
-                        type=int,
-                        default=2,
-                        help='Samples per second of input')
-    parser.add_argument('--data_dir',
-                        type=str,
-                        default='../../../../build/derived_data/ECG/',
-                        help='Path to heart rate data for reading')
-    parser.add_argument(
-        '--model',
-        type=str,
-        default='../../../../build/derived_data/apnea/models/two_ar5_masked',
-        help='Path to model')
-    parser.add_argument('--format',
-                        type=str,
-                        default='{0}/{1}_self_AR3/heart_rate',
-                        help='Map from (data_dir,name) to file of heart_rates')
+                        help='Path to statistics for pass1')
     parser.add_argument('--show',
                         action='store_true',
                         help="display figure using Qt5")
-    parser.add_argument('fig_path', type=str, help="path to figure")
+    parser.add_argument('--fig_path', type=str, help="path to figure")
     utilities.common_arguments(parser)
     args = parser.parse_args(argv)
-    args.sample_rate_in *= PINT('Hz')
     utilities.join_common(args)
     return args
 
 
+def average_psd(psds, names):
+    psd_list = [psds[name] for name in names]
+    return sum(psd_list) / len(psd_list)
+
+
+def plot_coefficients(axes, threshold_statistics, pass1_statistics):
+    """Plot coefficients for threshold and pass1.
+    """
+
+    def _plot(statistics, label, key, color, factor):
+        axes.plot(statistics['frequencies'],
+                  statistics[key].reshape(-1) * factor,
+                  label=label,
+                  color=color,
+                  linewidth=1,
+                  linestyle='solid')
+
+    _plot(threshold_statistics, 'threshold/100', 'threshold_coefficients', 'g',
+          0.01)
+    _plot(pass1_statistics, 'pass1', 'pass1_coefficients', 'k', 1.0)
+    axes.set_xlabel('frequency/cpm')
+    axes.legend()
+
+
+def plot_averages(axes, statistics, prefix):
+    """Plot average psd for a records and for c records.
+    """
+    frequencies = statistics['frequencies']
+
+    psds = dict(
+        (name, numpy.log10(psd)) for name, psd in statistics['psds'].items())
+    a_names = []
+    c_names = []
+    for name in psds.keys():
+        if name[0] == 'a':
+            a_names.append(name)
+        if name[0] == 'c':
+            c_names.append(name)
+    c_names.remove('c04')  # Severe arrhythmia in c04
+
+    for (names, label, color) in ((a_names, f'{prefix} a_avg', 'r'),
+                                  (c_names, f'{prefix} c_avg', 'b')):
+        axes.plot(frequencies,
+                  average_psd(psds, names),
+                  label=label,
+                  color=color,
+                  linewidth=1,
+                  linestyle='solid')
+    axes.set_xlabel('frequency/cpm')
+    axes.set_ylabel('log_10 PSD')
+    axes.legend()
+
+
+def plot_thresholds(axes, statistics):
+    """For each record, plot fit threshold vs best  threshold.
+
+    For visualizing fit threshold performance
+
+    """
+    threshold_coefficients = statistics['threshold_coefficients'].reshape(-1)
+    for name, psd_ in statistics['psds'].items():
+        psd = numpy.log10(psd_)
+        best_threshold = statistics['best_threshold'][name]
+        fit_ = numpy.dot(threshold_coefficients, psd)
+        fit_threshold = min(2.0, max(-2.0, fit_))
+        color = {'a': 'r', 'b': 'g', 'c': 'b', 'x': 'k'}[name[0]]
+        if name in statistics['args'].record_names:
+            color = 'm'
+        axes.plot(best_threshold,
+                  fit_threshold,
+                  marker=f'${name}$',
+                  color=color,
+                  markersize=14,
+                  linestyle='None')
+    axes.set_ylabel('fit threshold')
+    axes.set_xlabel('best threshold')
+
+
+def plot_pass1(axes,
+               threshold_statistics,
+               pass1_statistics,
+               pass1_threshold=0.175):
+    """Plot each record in (threshold_coefficients,
+    pass1_coefficients) coordinates.
+
+
+
+    For visualizing pass1 performance
+    """
+
+    pass1_coefficients = pass1_statistics['pass1_coefficients'].reshape(-1)
+    threshold_coefficients = threshold_statistics[
+        'threshold_coefficients'].reshape(-1)
+    print('     best   fit pass1')
+    for name, psd_ in threshold_statistics['psds'].items():
+        threshold_psd = numpy.log10(psd_)
+        pass1_psd = numpy.log10(pass1_statistics['psds'][name])
+        pass1 = numpy.dot(pass1_coefficients, pass1_psd)
+        fit_ = numpy.dot(threshold_coefficients, threshold_psd)
+        threshold = min(2.0, max(-2.0, fit_))
+        color = {'a': 'r', 'b': 'g', 'c': 'b', 'x': 'k'}[name[0]]
+        axes.plot(pass1,
+                  threshold,
+                  marker=f'${name}$',
+                  color=color,
+                  markersize=14,
+                  linestyle='None')
+        best_threshold = threshold_statistics['best_threshold'][name]
+        print(
+            f'{name} {best_threshold:5.2f} {threshold:5.2f} {pass1-pass1_threshold:5.2f}'
+        )
+    axes.plot([pass1_threshold, pass1_threshold], [-2.1, 2.1], color='k')
+    axes.set_ylabel('fit threshold')
+    axes.set_xlabel('pass1 statistic')
+
+
 def main(argv=None):
+    """Call plotting functions for statistics
+
+    """
 
     if argv is None:
         argv = sys.argv[1:]
     args, _, pyplot = plotscripts.utilities.import_and_parse(parse_args, argv)
 
-    trouble = 'a11 c10'.split()
-    colors = ['r', 'b']
-    fig, axeses = pyplot.subplots(nrows=2, ncols=2, figsize=(6, 4))
+    with open(args.pickle_threshold, 'rb') as _file:
+        threshold_statistics = pickle.load(_file)
+
+    with open(args.pickle_pass1, 'rb') as _file:
+        pass1_statistics = pickle.load(_file)
+
+    fig, axeses = pyplot.subplots(nrows=2, ncols=2, figsize=(6, 8))
+
+    plot_averages(axeses[0, 0], pass1_statistics, 'pass1')
+    plot_averages(axeses[0, 0], threshold_statistics, 'threshold')
+    plot_coefficients(axeses[1, 0], threshold_statistics, pass1_statistics)
+    plot_pass1(axeses[0, 1], threshold_statistics, pass1_statistics)
+    plot_thresholds(axeses[1, 1], threshold_statistics)
+
     fig.tight_layout()
-
-    records = dict(
-        (name, utilities.Pass1(name, args))
-        for name in args.A_names + args.C_names + args.X_names + args.B_names)
-    frequencies = records[args.A_names[0]].frequencies
-
-    def plot_psd(names, label, color):
-        psd_list = [records[name].psd for name in names]
-        for name in names:
-            assert numpy.array_equal(frequencies, records[name].frequencies)
-        psd_average = sum(psd_list) / len(psd_list)
-        for axes in axeses[0, :2]:
-            axes.plot(frequencies,
-                      numpy.log(psd_average),
-                      label=label,
-                      color=color,
-                      linewidth=3,
-                      linestyle='solid')
-        #for psd in psd_list:
-        #    axeses[0].plot(frequencies, numpy.log(psd), color=color, linestyle='dotted')
-
-    plot_psd(args.A_names, r'$\bar a$', 'r')
-    plot_psd(args.C_names, r'$\bar c$', 'b')
-    for axes in axeses[0, :2]:
-        for name, color in zip(trouble, colors):
-            axes.plot(frequencies,
-                      numpy.log(records[name].psd),
-                      color=color,
-                      linestyle='dotted',
-                      label=name)
-        axes.set_xlabel('frequency/cpm')
-        axes.set_ylabel('log psd')
-
-    axeses[0, 1].set_xlim(0.5, 4.0)
-    axeses[0, 1].set_ylim(0, 3.5)
-    for axes in axeses[0, :]:
-        axes.legend()
-
-    def plot_stats(axes, names, color):
-        for name in names:
-            record = records[name]
-            axes.plot(record.statistic_1(),
-                      record.statistic_2(),
-                      color=color,
-                      marker=f'${name}$',
-                      markersize=14,
-                      linestyle='None')
-
-    for axes in axeses[1, :]:
-        plot_stats(axes, args.A_names, 'r')
-        plot_stats(axes, args.C_names, 'b')
-        plot_stats(axes, args.B_names, 'g')
-        plot_stats(axes, args.X_names, 'k')
-        axes.set_xlabel(r'$F(PSD)$')
-        axes.set_ylabel(r'$G(PSD)$')
-        y = numpy.linspace(0, 5000, 2)
-        x = .36 - y * 0.
-        axes.plot(x, y, linestyle='dotted')
-    axeses[1, 1].set_xlim(0.307, 0.417)
-    axeses[1, 1].set_ylim(200, 1500)
     if args.show:
         pyplot.show()
-    fig.savefig(args.fig_path)
+    if args.fig_path:
+        fig.savefig(args.fig_path)
 
     return 0
 
--- src/hmmds/applications/apnea/threshold_statistics.py
+++ src/hmmds/applications/apnea/threshold_statistics.py
@@ -1,6 +1,16 @@
-"""threshold_statistics.py Calculate statistics of records for map to threshold
+"""fft_threshold_statistics.py Calculate statistics of records and map to threshold
 
-python threshold_statistics.py default hight_classless low_classless result.pkl a01 a02 ...
+python fft_threshold_statistics.py $(MODELS)/default $@ $(APLUSNAMES)
+
+Write pickle file containing:
+
+    args.record_names: Records used for fit
+
+    best_thresholds: Dict of thresholds for each a, b, c record.
+
+    fit_thresholds: Dict of fit thresholds for each a, b, c, x record
+
+    Leave one out error as a function of SVD rank
 
 """
 from __future__ import annotations
@@ -11,8 +21,14 @@ import typing
 import pickle
 
 import numpy
+import numpy.linalg
+import scipy.signal
+import sklearn.svm
+import sklearn.pipeline
+import sklearn.preprocessing
 
 import utilities
+import hmmds.applications.apnea.model_init
 
 
 def parse_args(argv):
@@ -25,12 +41,18 @@ def parse_args(argv):
     parser.add_argument('--resolution',
                         type=float,
                         nargs=3,
-                        default=(1.0e-4, 1.0e4, 10),
+                        default=(-3.0, 3.0, 100),
                         help="geometric range of thresholds")
-    parser.add_argument('a_model_path', type=str, help="path to model")
-    parser.add_argument('high_model_path', type=str, help="path to model")
-    parser.add_argument('low_model_path', type=str, help="path to model")
-    parser.add_argument('result_path', type=str, help="path to pickle file")
+    parser.add_argument('--result_path', type=str, help="path to pickle file")
+    parser.add_argument('--leave_one_out',
+                        action='store_true',
+                        help='In turn, hold each record out from fit')
+    parser.add_argument('--all_in',
+                        action='store_true',
+                        help='Report performance on each training record')
+    parser.add_argument('classification_model_path',
+                        type=str,
+                        help="path to model")
     parser.add_argument('record_names', type=str, nargs='+')
     args = parser.parse_args(argv)
     utilities.join_common(args)
@@ -42,88 +64,215 @@ def parse_args(argv):
 
 class ModelRecord(utilities.ModelRecord):
 
-    def __init__(self: ModelRecord, model_path: str, record_name: str):
-        """Assign values of statistics to self.
+    def __init__(self: ModelRecord, args, record_name: str):
+        """
 
         Args:
             model_path: Path to HMM.  Observation models include 'class'
             record_name: EG, 'a01'
+            psd: For estimating threshold
         """
+        model_path = args.classification_model_path
         utilities.ModelRecord.__init__(self, model_path, record_name)
+        # Read the raw heart rate
+        path = args.heart_rate_path_format.format(record_name)
+        with open(path, 'rb') as _file:
+            _dict = pickle.load(_file)
+        assert set(_dict.keys()) == set('hr sample_frequency'.split())
+        assert _dict['sample_frequency'].to('Hz').magnitude == 2
 
-        # Calculate statistics
-        self.pass1 = utilities.Pass1(record_name, self.model.args).statistic_1()
+        self.hr_sample_frequency = _dict['sample_frequency']
+        self.raw_hr = _dict['hr'].to('1/minute').magnitude
+        self.threshold = None
+        self.second_derivative = None
 
-        if self.has_class:
-            class_model = self.model.y_mod['class']  # Save
-            del self.model.y_mod['class']
-            #self.weights = self.model.weights(self.y_raw_data).sum(axis=0)
-            p_yt_steps = self.model.likelihood(self.y_raw_data[0])
-            self.model.y_mod['class'] = class_model  # Restore for future use
-        else:
-            #self.weights = self.model.weights(self.y_raw_data).sum(axis=0)
-            p_yt_steps = self.model.likelihood(self.y_raw_data[0])
-        self.log_likelihood = numpy.log(p_yt_steps).sum()
+    def get_best_threshold(self: ModelRecord, low, high, levels):
+        """Calculate and return best threshold.
 
-    def get_threshold(self, low, high, levels):
-        self.threshold, self.counts = self.best_threshold(low, high, levels)
+        Also assign to self.second_derivative estimate of d^2 / d threshold^2
+
+        Here threshold is log_10(x) where x is used at the lowest level
+        """
+        if self.threshold is None:
+            self.threshold, self.counts = self.best_threshold(low, high, levels)
+            exp_threshold = 10.0**self.threshold
+            self.classify(threshold=exp_threshold * 10)
+            plus_counts = self.score()
+            self.classify(threshold=exp_threshold / 10)
+            minus_counts = self.score()
+            self.second_derivative = (plus_counts[1:3].sum(
+            ) + minus_counts[1:3].sum()) / 1 - self.counts[1:3].sum()
+
         return self.threshold
+
+
+def psd(args, name):
+    """Estimate the power spectral density for named record
+    """
+    with open(args.heart_rate_path_format.format(name), 'rb') as _file:
+        _dict = pickle.load(_file)
+        sample_frequency = _dict['sample_frequency'].to('1/minutes').magnitude
+        hr = _dict['hr']
+        trim = int(sample_frequency * args.trim_start.to('minutes').magnitude)
+    heart_rate = _dict['hr'].to('1/minute').magnitude[trim:-trim]
+    frequencies, psd_ = scipy.signal.welch(heart_rate,
+                                           fs=sample_frequency,
+                                           nperseg=args.fft_width)
+    return psd_, frequencies
+
+
+def svm(psd_dict, a_names, c_names):
+    """Find maximum margin separating hyperplane for pass1
+    """
+
+    n_samples = len(a_names) + len(c_names)
+    n_features = len(psd_dict[a_names[0]])
+    X = numpy.empty((n_samples, n_features))
+    y = numpy.empty(n_samples)
+    class_dict = {'a': 1, 'c': 0}
+    for i, name in enumerate(a_names + c_names):
+        X[i, :] = psd_dict[name]
+        y[i] = class_dict[name[0]]
+    clf = sklearn.pipeline.make_pipeline(
+        sklearn.preprocessing.StandardScaler(),
+        sklearn.svm.LinearSVC(random_state=0, tol=1e-5, dual=False))
+    clf.fit(X, y)
+
+    coefficients = clf.named_steps['linearsvc'].coef_
+    return coefficients
+
+
+def leave_one_out(args, model_records, psd_dict):
+    """For each record in model_records, fit leaving that record out
+    and test on that record.
+
+    """
+    for rank in range(1, len(args.record_names)):
+        error_sum = 0
+        for name in args.record_names:
+            names = args.record_names.copy()
+            names.remove(name)
+            fit = Fit(names, model_records, rank, psd_dict)
+            threshold = fit.threshold(psd_dict[name])
+            model_records[name].classify(threshold=10**threshold)
+            counts = model_records[name].score()
+            error_sum += counts[1:3].sum()
+        print(f'{rank=:2d} {error_sum=}')
 
 
 class Fit:
 
-    def __init__(self, names: list, a_model_record: dict,
-                 high_model_record: dict, low_model_record: dict, low, high,
-                 levels):
-        """Fit affine model for threshold
+    def __init__(self: Fit, names, model_records, rank, psd_dict):
+        """Fit linear model for threshold
+
         Args:
-            names: Records to use for fitting parameters
-            a_model_record: Eg, a_model_record["a01"].model was fit to APLUSNAMES
-            high_model_record: Eg, high_model_record["a01"].model was fit records with high thresholds
-            low_model_record: Eg, high_model_record["a01"].model was fit records with low thresholds
+            args: 
         
         Use numpy.linalg.lstsq(a,b) to compute the vector x that
         approximately solves the equation a @ x = b where
 
-        #a[i,j] is for record i and j = [Pass1, @p_states]
-        a[i,j] is for record i and j = [Pass1, log_like_a, log_like_c, 1]
+        a[i,:] = psd for record i
 
         b[i] is log(best_threshold) for record i
 
 
         """
-        a = numpy.empty((len(names), 4))
-        b = numpy.empty(len(names))
+
+        self.fit = {}
+
+        n_names = len(names)
+        n_frequencies = len(psd_dict[names[0]])
+        psds = numpy.empty((n_names, n_frequencies))
+        t_best = numpy.empty(n_names)
+        H = numpy.zeros((n_names, n_names))
         for i, name in enumerate(names):
-            a[i, 0] = a_model_record[name].pass1
-            a[i, 1] = high_model_record[name].log_likelihood
-            a[i, 2] = low_model_record[name].log_likelihood
-            a[i, 3] = 1
-            b[i] = numpy.log(a_model_record[name].get_threshold(
-                low, high, levels))
-        self.coefficients = numpy.linalg.lstsq(a, b, rcond=None)[0]
+            psds[i, :] = numpy.log10(psd_dict[name])
+            t_best[i] = model_records[name].threshold
+            H[i, i] = model_records[name].second_derivative
 
-    def model_record2a(self, model_record):
-        """
-        """
-        result = numpy.empty(self.n_states + 1)
-        weights = model_record.weights
-        assert weights.shape == (self.n_states,)
-        result[1:] = weights / weights.sum()
-        result[0] = model_record.pass1
-        return result
+        U, S, VT = numpy.linalg.svd(H @ psds)
+        error = f'{U.shape=} {S.shape=} {VT.shape=}'
+        assert U.shape == (n_names, n_names), error
+        assert S.shape == (n_names,), error
+        assert VT.shape == (n_frequencies, n_frequencies), error
+        '''The rows of VT are directions in PSD space.  The columns of
+        U are directions in record space.
 
-    def threshold(self, a_model_record, high_model_record, low_model_record):
-        """Calculate estimated threshold as affine function of
-        statistics of a record
+        '''
+        self.coefficients = VT.T[:, :rank] @ (U[:, :rank] /
+                                              S[:rank]).T @ H @ t_best
+        assert self.coefficients.shape == (n_frequencies,)
+
+        for name in names:
+            self.fit[name] = self.threshold(psd_dict[name])
+
+    def threshold(self: Fit, psd_):
+        """Calculate estimated threshold as linear function of
+        a psd.
 
         """
-        a = numpy.empty(4)
-        a[0] = a_model_record.pass1
-        a[1] = high_model_record.log_likelihood
-        a[2] = low_model_record.log_likelihood
-        a[3] = 1
-        return numpy.exp(a @ self.coefficients)
+        high = 3.0
+        low = -3.0
+        result = numpy.log10(psd_) @ self.coefficients
+        return min(high, max(low, result))
+
+
+def all_in(args, model_records, fit):
+    """ Print error counts for records in args.record_names
+    """
+    total_errors = 0
+    print('''In fit_threshold_statistics.all_in
+name best  fit   difference N2N N2A A2N A2A''')
+    for name in args.record_names:
+        best = model_records[name].threshold
+        _fit = fit.fit[name]
+        model_records[name].classify(threshold=10**_fit)
+        counts = model_records[name].score()
+        total_errors += counts[1:3].sum()
+        print(
+            f'{name} {best:5.2f} {_fit:5.2f} {best-_fit:5.2f}       {counts[0]:3d} {counts[1]:3d} {counts[2]:3d} {counts[3]:3d}'
+        )
+    print(f'{total_errors=}')
+
+
+def calculate_statistics(args, model_records, fit, psd_dict, frequencies):
+    """Create and return a dict of statistics containing:
+
+    result['args'] Command line args
+    result['best_threshold'] Best threshold for each of the 70
+        records.  Obtained by cheating for training records.  Value is
+        0.0 for x-files
+    result['psds'] A psd for each of the 70 records
+    result['frequencies'] The frequencies for each bin in the psds
+    result['threshold_coeficients'] v with threhsold[name] = v @ psd[name]
+    result['pass1_coefficients'] v @ psd > pass1_threshold -> A or N
+
+    """
+
+    result = {}
+    result['args'] = args
+    best_thresholds = result['best_threshold'] = {}
+    result['psds'] = psd_dict
+    result['frequencies'] = frequencies
+    for name in args.all_names:
+        if name[0] == 'x':
+            best_thresholds[name] = 0.0
+        else:
+            best_thresholds[name] = model_records[name].get_best_threshold(
+                args.low, args.high, args.levels)
+        assert isinstance(best_thresholds[name], float)
+        #print(f"{name} {result['threshold']['best'][name]=:5.2f} {result['threshold']['fit'][name]=:5.2f}")
+    result['threshold_coefficients'] = fit.coefficients
+    a_names = []
+    c_names = []
+    for name in model_records.keys():
+        if name[0] == 'a':
+            a_names.append(name)
+        if name[0] == 'c':
+            c_names.append(name)
+    c_names.remove('c04')  # Severe arrhythmia in c04
+    result['pass1_coefficients'] = svm(psd_dict, a_names, c_names)
+    return result
 
 
 def main(argv=None):
@@ -132,42 +281,39 @@ def main(argv=None):
 
     """
 
+    numpy.seterr(divide='raise', invalid='raise')
     if argv is None:  # Usual case
         argv = sys.argv[1:]
 
     args = parse_args(argv)
+    test_names = args.a_names + args.b_names + args.c_names
 
-    a_model_record = {}
-    high_model_record = {}
-    low_model_record = {}
-    for name in args.all_names:
-        a_model_record[name] = ModelRecord(args.a_model_path, name)
-        high_model_record[name] = ModelRecord(args.high_model_path, name)
-        low_model_record[name] = ModelRecord(args.low_model_path, name)
+    # Calculate a psd for each record.  Some might be unnecessary, but
+    # they are cheap to calculate.
+    psds = dict((name, psd(args, name)[0]) for name in args.all_names[1:])
+    first_name = args.all_names[0]
+    psds[first_name], frequencies = psd(args, first_name)
 
-    fit = Fit(args.record_names, a_model_record, high_model_record,
-              low_model_record, args.low, args.high, args.levels)
+    # Make a ModelRecord for each record with expert annotations.  It
+    # is expensive because of the search for the best threshold.
+    model_records = {}
+    for name in test_names:
+        model_records[name] = ModelRecord(args, name)
+        # Assign threshold, counts, and second_derivative to model_records[name]
+        model_records[name].get_best_threshold(args.low, args.high, args.levels)
 
-    statistics = {}
-    for name in args.all_names:
-        pass1 = a_model_record[name].pass1
-        high_log_likelihood = high_model_record[name].log_likelihood
-        low_log_likelihood = low_model_record[name].log_likelihood
-        fit_threshold = fit.threshold(
-            a_model_record[name],
-            high_model_record[name],
-            low_model_record[name],
-        )
-        if name[0] == 'x':
-            best_threshold = 1.0
-        else:
-            best_threshold = a_model_record[name].get_threshold(1e-4, 1e4, 10)
-        statistics[name] = numpy.array([
-            pass1, high_log_likelihood, low_log_likelihood, 1.0, fit_threshold,
-            best_threshold
-        ])
-    with open(args.result_path, 'wb') as _file:
-        pickle.dump((args.record_names, fit, statistics), _file)
+    if args.leave_one_out:
+        leave_one_out(args, model_records, psds)
+    if args.all_in or args.result_path:
+        fit = Fit(args.record_names, model_records, len(args.record_names),
+                  psds)
+    if args.all_in:
+        all_in(args, model_records, fit)
+    if args.result_path:
+        statistics = calculate_statistics(args, model_records, fit, psds,
+                                          frequencies)
+        with open(args.result_path, 'wb') as _file:
+            pickle.dump(statistics, _file)
 
     return 0
 
