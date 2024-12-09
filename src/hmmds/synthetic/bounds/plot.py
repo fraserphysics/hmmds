cd8cc2bed0a14c339ef93ba866b8db0a096baceb
Andrew <andy@fraserphysics.com>
2024-12-09 09:25:19 -0700

    Plot boxes and Delta in SVD coordinates for particles close to true.

--- src/hmmds/synthetic/bounds/plot.py
+++ src/hmmds/synthetic/bounds/plot.py
@@ -9,6 +9,7 @@ import argparse
 import pickle
 
 import numpy
+import numpy.linalg
 import matplotlib
 import matplotlib.colors
 
@@ -69,6 +70,37 @@ def plot_point(axes, x, color, label=None):
               label=label)
 
 
+class Close:
+
+    def __init__(self, true_x, particles):
+        self.true_x = true_x
+        self.particles = particles
+        self.distances = numpy.asarray(
+            [self.distance(particle) for particle in particles])
+        self.indices = numpy.argsort(self.distances)
+
+    def distance(self, particle):
+        """Max norm in coordinates self.box from true_x to particle
+        """
+        delta = particle.x - self.true_x
+        return numpy.abs(numpy.linalg.solve(particle.box, delta)).max()
+
+    def nth(self, n):
+        """Return the distance and particle that are nth closest
+
+        """
+        index = self.indices[n]
+        return self.distances[index], self.particles[index]
+
+    def delta_svd(self, n):
+        """Calculate (true_x - nth closest) in svd coordinates
+        """
+        particle = self.particles[self.indices[n]]
+        U, S, VT = numpy.linalg.svd(particle.box)
+        delta = self.true_x - particle.x
+        return numpy.dot((U / S).T, delta)
+
+
 def main(argv=None):
     """Plot some stuff
     """
@@ -83,11 +115,7 @@ def main(argv=None):
     clouds = dict_in['clouds']
 
     colors = list(matplotlib.colors.TABLEAU_COLORS.values())
-    figure, axeses = pyplot.subplots(nrows=5,
-                                     ncols=3,
-                                     figsize=(5, 9),
-                                     sharex='row',
-                                     sharey='row')
+    figure, axeses = pyplot.subplots(nrows=5, ncols=3, figsize=(5, 9))
 
     for i in range(args.start, args.start + 5):
         if (i, 'forecast') not in clouds:
@@ -98,43 +126,49 @@ def main(argv=None):
         else:
             update = []
 
-        # Plot boxes in leftmost column
+        close = Close(x_all[i], forecast)
+        delta_svd = close.delta_svd(0)
         axes = axeses[i % 5, 2]
-        for particle in update:
-            if particle.parent == args.parent:
-                plot_box(axes, particle)
+        axes.plot(delta_svd)
 
-        # Plot forecast and update
+        # Plot points of forecast and update
         for j, cloud in enumerate((forecast, update)):
             if len(cloud) == 0:
                 continue
             axes = axeses[i % 5, j]
+            # Mark the "true" state with an x
+            axes.plot(x_all[i, 0], x_all[i, 2], marker='x')
             for particle in cloud:
                 color = colors[particle.parent % len(colors)]
                 plot_point(axes, particle.x, color)
 
-            if j == 0:  # Display information about forecast in legend
-                plot_point(axes,
-                           cloud[0].x,
-                           color,
-                           label=f'n={len(cloud)} y[{i}]={y_q[i]}')
-                axes.legend(loc='upper right')
-
-            # Mark the "true" state with an x
-            axes.plot(x_all[i, 0], x_all[i, 2], marker='x')
+                # Plot boxes
+                if len(cloud) > 1:
+                    _, next_closest = close.nth(1)
+                    plot_box(axes, next_closest)
+                distance, closest = close.nth(0)
+                plot_box(axes, closest)
 
             axes.set_xlim(-22, 22)
             axes.set_ylim(0, 50)
+        # Print information in legend of leftmost plot
+        axes = axeses[i % 5, 0]
+        plot_point(axes,
+                   closest.x,
+                   'red',
+                   label=f'y[{i}]={y_q[i]} {distance=:.3f} n={len(forecast)} ')
+        axes.legend(loc='upper right')
 
-        U, S, VT = numpy.linalg.svd(forecast[0].box)
-        print(f'{i} {S=}')
+        U, S, VT = numpy.linalg.svd(closest.box)
+        numpy.set_printoptions(precision=2)
+        print(f'{i} {distance=:.2f} {S=}')
+        numpy.set_printoptions(precision=8)
 
-    # Print box for forecast in last cloud
+    # Print box for closest
     if args.print_box:
-        particle = forecast[0]
-        print(f'box=\n{particle.box}')
-        print(f'neighbor={particle.neighbor.T}')
-        print(f'{particle.parent=}')
+        print(f'box=\n{closest.box}')
+        print(f'neighbor={closest.neighbor.T}')
+        print(f'{closest.parent=}')
     pyplot.show()
     return 0
 
