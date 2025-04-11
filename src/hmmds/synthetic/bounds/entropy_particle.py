a810362330c8f9eea6554001d540a6b691e0418f
Andrew <andy@fraserphysics.com>
2025-04-10 21:25:56 -0600

    Plot entropy estimate and optionally likelihood and/or counts.

--- src/hmmds/synthetic/bounds/entropy_particle.py
+++ src/hmmds/synthetic/bounds/entropy_particle.py
@@ -24,6 +24,7 @@ def parse_args(argv):
 python entropy_particle.py --dir_template study_threshold5k/{0} 1e-2 3e-3 1e-3'''
                                     )
     parser.add_argument('--plot_counts', action='store_true')
+    parser.add_argument('--plot_likelihood', action='store_true')
     parser.add_argument('--dir_template',
                         type=str,
                         default='study_threshold5k/{0}',
@@ -36,7 +37,7 @@ python entropy_particle.py --dir_template study_threshold5k/{0} 1e-2 3e-3 1e-3''
     return args
 
 
-def plot_key(args, axeses, key):
+def plot_key(args, axes_dict, key):
     """Plot data from args.dir_template(key) on axeses
 
     Args:
@@ -44,10 +45,6 @@ def plot_key(args, axeses, key):
         dict_template: EG, study_threshold/{0}/dict.pkl
         keys: EG, e-2 1e-3 1e-4
     """
-    if args.plot_counts:
-        n_rows = 3
-    else:
-        n_rows = 2
 
     with open(os.path.join(args.dir_template.format(key), 'dict.pkl'),
               'rb') as file_:
@@ -60,16 +57,19 @@ def plot_key(args, axeses, key):
     entropy = -cum_sum / numpy.arange(1, len(cum_sum) + 1) / 0.15
     reference = numpy.ones(len(entropy)) * 0.906
     x = numpy.arange(offset, len(gamma))
-    axeses[n_rows - 2].plot(numpy.log10(gamma), label=f'{key}')
-    axeses[n_rows - 2].set_ylabel(r'log$_{10}(P(y[t]|y[0:t]))$')
-    axeses[n_rows - 1].plot(x, entropy, label=f'{key}')
-    axeses[n_rows - 1].plot(x, reference)
-    axeses[n_rows - 1].set_xlabel(r'$t$')
-    axeses[n_rows - 1].set_ylabel(r'$\hat h$')
+
+    axes_dict['entropy'].plot(x, entropy, label=f'{key}')
+    axes_dict['entropy'].plot(x, reference)
+    axes_dict['entropy'].set_xlabel(r'$t$')
+    axes_dict['entropy'].set_ylabel(r'$\hat h$')
     h_hat = entropy[-1]
     print(f'h_hat({key})= {h_hat} {(h_hat/0.906-1)*100:4.2f}%')
 
-    if not args.plot_counts:
+    if 'likelihood' in axes_dict:
+        axes_dict['likelihood'].plot(numpy.log10(gamma), label=f'{key}')
+        axes_dict['likelihood'].set_ylabel(r'log$_{10}(P(y[t]|y[0:t]))$')
+
+    if 'counts' not in axes_dict:
         return
     n_forecast = numpy.zeros(len(gamma), dtype=int)
     n_update = numpy.zeros(len(gamma), dtype=int)
@@ -81,8 +81,8 @@ def plot_key(args, axeses, key):
                 n_update[n] = len(numpy.load(file_))
             except:
                 break
-    axeses[n_rows - 3].plot(n_forecast, label=f'n_forecast({key})')
-    axeses[n_rows - 3].plot(n_update, label=f'n_update({key})')
+    axes_dict['counts'].plot(n_forecast, label=f'n_forecast({key})')
+    axes_dict['counts'].plot(n_update, label=f'n_update({key})')
     argmin = n_update[100:].argmin() + 100
     print(
         f'Minimum of update for {key}: n_update[{argmin}]={n_update[argmin]}\n')
@@ -95,24 +95,37 @@ def main(argv=None):
         argv = sys.argv[1:]
     args = parse_args(argv)
 
+    n_rows = 1
     if args.plot_counts:
-        n_rows = 3
-    else:
-        n_rows = 2
-    figure, axeses = pyplot.subplots(nrows=n_rows, ncols=1, sharex=True)
+        n_rows += 1
+    if args.plot_likelihood:
+        n_rows += 1
+    figure, axes_array = pyplot.subplots(nrows=n_rows, ncols=1, sharex=True)
+    if n_rows == 1:
+        axes_array = [axes_array]
+    row_count = n_rows - 1
+    axes_dict = {'entropy': axes_array[row_count]}
+    if args.plot_counts:
+        row_count -= 1
+        axes_dict['counts'] = axes_array[row_count]
+    if args.plot_likelihood:
+        row_count -= 1
+        axes_dict['likelihood'] = axes_array[row_count]
+
     for key in args.keys:
-        plot_key(args, axeses, key)
+        plot_key(args, axes_dict, key)
 
-    axeses[n_rows - 2].set_ylabel(r'log$_{10}(P(y[t]|y[0:t]))$')
-    axeses[n_rows - 2].legend()
+    axes_dict['entropy'].set_xlabel(r'$t$')
+    axes_dict['entropy'].set_ylabel(r'$\hat h$')
+    axes_dict['entropy'].legend()
 
-    axeses[n_rows - 1].set_xlabel(r'$t$')
-    axeses[n_rows - 1].set_ylabel(r'$\hat h$')
-    axeses[n_rows - 1].legend()
+    if 'counts' in axes_dict:
+        axes_dict['counts'].set_ylabel(r'N')
+        axes_dict['counts'].legend()
 
-    if args.plot_counts:
-        axeses[n_rows - 3].set_ylabel(r'N')
-        axeses[n_rows - 3].legend()
+    if 'likelihood' in axes_dict:
+        axes_dict['likelihood'].set_ylabel(r'log$_{10}(P(y[t]|y[0:t]))$')
+        axes_dict['likelihood'].legend()
 
     pyplot.show()
     return 0
